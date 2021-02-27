9a980b886899b767c316012ef9f8c4ad484b2476
Boguslav13 <53686122+Boguslav13@users.noreply.github.com>
2021-02-28 00:30:41 +0300

    Add files via upload

--- js/Carousel-move_for_some_px.js
+++ js/Carousel-move_for_some_px.js
@@ -1,1 +1,32 @@
+function SlideShow( id , step )
+{
+	let Sliders = document.getElementById( id ).querySelectorAll( 'div.Slide' );
+	let SlidersCount = Sliders.length - 1;
 
+	if( step == '-1' )
+	{
+		Sliders[ 2 ].style.transform = 'translateX(' + 255 + 'px)';
+		Sliders[ 1 ].style.transform = 'translateX(' + 255 + 'px)';
+		Sliders[ 0 ].style.transform = 'translateX(' + 255 + 'px)';
+		setTimeout( function(){
+			document.getElementById( id ).insertBefore( Sliders[ SlidersCount ] , document.getElementById( id ).childNodes[ 0 ] );
+			Sliders[ 2 ].style.transform = 'translateX(' + '0' + 'px)';
+			Sliders[ 1 ].style.transform = 'translateX(' + '0' + 'px)';
+			Sliders[ 0 ].style.transform = 'translateX(' + '0' + 'px)';
+		} , 1000 );
+	} else {
+		Sliders[ 0 ].style.transform = 'translateX(' + '-255' + 'px)';
+		Sliders[ 1 ].style.transform = 'translateX(' + '-255' + 'px)';
+		Sliders[ 2 ].style.transform = 'translateX(' + '-255' + 'px)';
+		Sliders[ 3 ].style.transform = 'translateX(' + '-255' + 'px)';
+		setTimeout( function(){
+			document.getElementById( id ).appendChild( Sliders[ 0 ] );
+			Sliders[ 0 ].style.transform = 'translateX(' + '0' + 'px)';
+			Sliders[ 1 ].style.transform = 'translateX(' + '0' + 'px)';
+			Sliders[ 2 ].style.transform = 'translateX(' + '0' + 'px)';
+			Sliders[ 3 ].style.transform = 'translateX(' + '0' + 'px)';
+		} , 1000 );
+	
+	}
+
+}
